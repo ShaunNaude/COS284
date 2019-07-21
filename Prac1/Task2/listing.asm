6a6c85dfc6971d23e313cfc23aa2332bbf77d47f
Shaun_Naude <shaunnaude20@gmail.com>
2019-07-21 09:08:21 +0200

    task 2

---
+++ Prac1/Task2/listing.asm
@@ -0,0 +1,9 @@
+        segment .data
+zero    dd      0.0
+neg0    dd      -0.0
+zero2    dd      0
+negZero2    dd  -0
+num1    dd      3.5651
+num2    dd      12.1
+num3    dd      -2.6
+num4    dd      1.5
