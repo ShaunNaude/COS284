114a20bc305bdb95b09d7af60f2fee61920f7c82
Shaun_Naude <shaunnaude20@gmail.com>
2019-07-21 08:12:53 +0200

    task 1

---
+++ Prac1/exit.asm
@@ -0,0 +1,9 @@
+segment .text
+global  _start
+_start:
+mov  eax,1 ;sys_write
+mov  ebx,-1 ;return code
+int  0x80 ; 32 bit //syscall is 64bit
+
+
+;The return code is going to be 255 because is interpreted as an unsigned char in C
